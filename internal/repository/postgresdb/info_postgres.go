704cc628ee19a2cb2cb0476c818c70c140685a53
andrew-nino <andrey.muravey1@gmail.com>
2024-07-05 00:21:43 +0300

    При получении данных по трудозатратам добавляется описание задачи. Исправлена ошибка получения значения в часахи минутах.

--- internal/repository/postgresdb/info_postgres.go
+++ internal/repository/postgresdb/info_postgres.go
@@ -3,6 +3,7 @@ package postgresdb
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/andrew-nino/em_time-tracker/entity"
 	"github.com/jmoiron/sqlx"
@@ -66,15 +67,27 @@ backward:
 func (i *InfoFromPostgres) GetUserEffort(user_id, beginningPeriod, endPeriod string) ([]entity.Effort, entity.People, error) {
 
 	effor := []entity.Effort{}
-	queryPeopleStr := fmt.Sprintf(`SELECT task_id, to_char( finished_at - created_at, 'HH24:MI') AS total_time 
-								   FROM %s
-								   WHERE created_at >= to_timestamp($1, 'YYYY-MM-DD') AND finished_at <= to_timestamp($2, 'YYYY-MM-DD') + interval '24 hour'
-								   GROUP BY people_id, task_id, total_time
-								   HAVING people_id = $3
-								   ORDER BY people_id, total_time DESC`, trackerTable)
+	queryPeopleStr := fmt.Sprintf(`SELECT trc.task_id, tsc.description, 
+								   EXTRACT( EPOCH FROM trc.finished_at - trc.created_at) AS total_time 
+								   FROM %s AS trc
+								   INNER JOIN %s  AS tsc ON tsc.id = trc.task_id
+								   WHERE trc.created_at >= to_timestamp($1, 'YYYY-MM-DD') AND trc.finished_at <= to_timestamp($2, 'YYYY-MM-DD') + interval '24 hour'
+								   GROUP BY trc.people_id, trc.task_id, total_time, tsc.description
+								   HAVING trc.people_id = $3
+								   ORDER BY trc.people_id, total_time DESC`, trackerTable, taskTable)
 	err := i.db.Select(&effor, queryPeopleStr, beginningPeriod, endPeriod, user_id)
 	if err != nil {
 		return nil, entity.People{}, err
+	}
+
+	for i := 0; i < len(effor); i++ {
+		effor[i].TotalTime, _, _ = strings.Cut(effor[i].TotalTime, ".")
+		h, err := strconv.Atoi(effor[i].TotalTime)
+		if err != nil {
+			return nil, entity.People{}, err
+		}
+
+		effor[i].TotalTime = fmt.Sprintf("%dh %dm", h/3600, h%3600/60)
 	}
 
 	var user entity.People
